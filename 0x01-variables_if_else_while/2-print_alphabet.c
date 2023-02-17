8242def9a39b7ec5ee8433d47108013fb9890244
123Nqobi <nqobiledubi@gmail.com>
2023-02-17 06:16:23 -0800

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,24 @@
-#include "holberton.h"
+/*
+ * File: 2-print_alphabet.c
+ * Auth: Brennan D Baraban
+ */
+
+#include <stdio.h>
 
 /**
- * main - check the code for ALX students
+ * main - Prints the alphabet in lowercase.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    print_alphabet();
-    return (0);
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
 }
+
