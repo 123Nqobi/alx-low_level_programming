cecbcba2df61a96408548b80f5af841eb983c007
123Nqobi <nqobiledubi@gmail.com>
2023-03-30 06:50:42 -0700

    task ad2

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,22 @@
-   global    main
-          extern    printf
-main:
-	  mov   edi, format
-	  xor   eax, eax
-	  call  printf
-	  mov 	eax, 0
-	  ret
-format: db `Hello, Holberton\n`,0
+SECTION .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
 
+	SECTION .text
+	extern printf
+	global main
+main:
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
+
+	mov eax, 0
+	ret
+
+Advanced task
+
+vi 
 #include <stdio.h>
 
 /**
@@ -20,3 +29,4 @@ void _attribute_ ((constructor)) bmain()
 	printf("You're beat! and yet, you must alllow");
 	Printf(",/nI bore my house upon my back!/n";
 }
+
